691db2967f5cfb9d7b3560f8b8613ab498df9738
Joseph Badlato <jbadlato@vt.edu>
2017-10-17 21:42:31 -0400

    Added animations to bio

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 $(function() {
-  $('#contact_form').submit(function(e) {
-    e.preventDefault();
-	$.ajax({
-	    url: "https://formspree.io/jbadlato@vt.edu",
-	    method: "POST",
-	    data: { 
-	    	'_subject': 'Portfolio Contact Form',
-	    	name: $('#name').val(),
-	    	'_replyto': $('#email').val(),
-	    	message: $('#message').val()
-	    },
-	    dataType: "json"
-	}).done(function(response) {
-	    $('#success_message').show();
-	    $('#contact_form').find("input[type=text], input[type=email], textarea").val("");
+
+	// CONTACT FORM -----------------------------------------
+	$('#contact_form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: "https://formspree.io/jbadlato@vt.edu",
+			method: "POST",
+			data: { 
+				'_subject': 'Portfolio Contact Form',
+				name: $('#name').val(),
+				'_replyto': $('#email').val(),
+				message: $('#message').val()
+			},
+			dataType: "json"
+		}).done(function(response) {
+			$('#success_message').show();
+			$('#contact_form').find("input[type=text], input[type=email], textarea").val("");
+		});
 	});
-  });
-  $(".title").hover(function(){
-  	$(this).css('color', '#000');
-  	var classList = $(this).attr('class').split(/\s+/);
-  	$.each(classList, function(index, item) {
-  		if (item === 'create') {
-  			$(".create").show();
-  		} else if (item === 'learn') {
-  			$(".learn").show();
-  		} else if (item === 'give_back') {
-  			$(".give_back").show();
-  		}
-  	});
-	},function(){
-		$(this).css('color', '#999');
-		$(".description").hide();
+
+	// BIO SECTION ------------------------------------------
+	$(".title").hover(function(){
+		var value = $(this).attr("class").split(/\s+/)[1];
+		var valueDisplay = $(".description." + value).css("display");
+		if (valueDisplay !== 'none') {
+			return;
+		}
+		$(".title").css('color', '#999');
+		$(this).css('color', '#000');
+		$(".description").hide('fast');
+		$(".description." + value).show('fast');
 	});
 });
